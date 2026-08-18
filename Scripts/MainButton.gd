d46ed22d1fa2e8881f49a27ee0b1a86169bed2ae
WillLeasure654 <will.leasure993@gmail.com>
2026-08-18 18:23:21 -0500

    Initial commit

---
+++ Scripts/MainButton.gd
@@ -0,0 +1,14 @@
+extends Button
+
+var label 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	label = get_node("../Label")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_pressed() -> void:
+	label.score += 1
