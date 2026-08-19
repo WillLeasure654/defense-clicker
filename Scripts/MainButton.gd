3c73c295b2b948fdc62faa4bb939c21b4584532f
garlicbread032 <ethanasaur@gmail.com>
2026-08-19 00:40:36 -0500

    Added button click animation

--- Scripts/MainButton.gd
+++ Scripts/MainButton.gd
@@ -1,5 +1,8 @@
 extends TextureButton
 
+var animation_playing = false
+
+@onready var anim = $ClickAnimation
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +15,13 @@ func _process(delta: float) -> void:
 
 func _on_pressed() -> void:
 	GameManager.money += 1
+	# Only play animation if one isn't already playing
+	if animation_playing:
+		anim.set_frame_and_progress(0, 0)
+
+	animation_playing = true
+	anim.play("Missile click")
+
+	await anim.animation_finished
+
+	animation_playing = false
