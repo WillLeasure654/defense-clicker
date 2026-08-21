6c72160790c14b725dca60535ff1cd53c5023176
Will Leasure <121632307+WillLeasure654@users.noreply.github.com>
2026-08-21 01:13:01 -0500

    spelling :(

--- Scripts/upgrade_4.gd
+++ Scripts/upgrade_4.gd
@@ -4,7 +4,7 @@ var cost = 2500
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	text = "New Govenrment Deal\nCost: " + str(round(cost))
+	text = "New Government Deal\nCost: " + str(round(cost))
 	pass # Replace with function body.
 
 
@@ -18,5 +18,5 @@ func _on_pressed() -> void:
 		GameManager.money -= cost
 		GameManager.passiveMoneyPerSec += 15
 		cost *= 1.2
-		text = "New Govenrment Deal\nCost: " + str(round(cost))
+		text = "New Government Deal\nCost: " + str(round(cost))
 	pass # Replace with function body.
--- Scripts/upgrade_6.gd
+++ Scripts/upgrade_6.gd
@@ -4,7 +4,7 @@ var cost = 62500
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	text = "Increace Political Tensions\nCost: " + str(round(cost))
+	text = "Increase Political Tensions\nCost: " + str(round(cost))
 	pass # Replace with function body.
 
 
@@ -18,5 +18,5 @@ func _on_pressed() -> void:
 		GameManager.money -= cost
 		GameManager.passiveMoneyPerSec += 100
 		cost *= 1.2
-		text = "Increace Political Tensions\nCost: " + str(round(cost))
+		text = "Increase Political Tensions\nCost: " + str(round(cost))
 	pass # Replace with function body.
